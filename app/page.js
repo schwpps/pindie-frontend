5a932cbe5e0fbb2f2550fa8669032663f0258127
schwpps <149660365+schwpps@users.noreply.github.com>
2024-05-30 10:18:27 +0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,18 +1,26 @@
+'use client';
+
+import { endpoints } from './api/config';
 import { Banner } from './components/Banner/Banner';
 import { CardsListSection } from './components/CardsListSection/CardsListSection';
 import { Promo } from './components/Promo/Promo';
-import { getNormalizedGamesDataByCategory } from './api/api-utils.js';
-import { endpoints } from './api/config';
+import { useGetDataByCategory } from './api/api-hooks';
+import { Preloader } from '@/app/components/Preloader/Preloader';
 
-export default async function Home() {
-    const popularGames = await getNormalizedGamesDataByCategory(endpoints.games, 'popular');
-    const newGames = await getNormalizedGamesDataByCategory(endpoints.games, 'new');
-
+export default function Home() {
+    const popularGames = useGetDataByCategory(endpoints.games, 'popular');
+    const newGames = useGetDataByCategory(endpoints.games, 'new');
     return (
         <main className="main">
             <Banner />
-            <CardsListSection id="popular" type="slider" title="Популярные" data={popularGames} />
-            <CardsListSection id="new" type="slider" title="Новинки" data={newGames} />
+            {popularGames && newGames ? (
+                <>
+                    <CardsListSection id="popular" title="Популярные" data={popularGames} type="slider" />
+                    <CardsListSection id="new" title="Новинки" data={newGames} type="slider" />
+                </>
+            ) : (
+                <Preloader />
+            )}
             <Promo />
         </main>
     );
